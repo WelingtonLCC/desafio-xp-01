2847645a422012e0669c63fe7373a25f89b0420c
Welington <welington.c.campos@hotmail.com>
2024-08-14 09:00:17 -0300

    Inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Content, Row, Column } from "./styles";
+import Input from "./components/Input"
+import Button from "./components/Button"
+import { useState } from "react";
 
 function App() {
+  const [currentNumber, setCurrentnumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('0');
+
+  const addOperation = (op) => {
+    if(op === '+' || op === '-' || op === '/' || op === '*' || op === '√'){
+      setCurrentnumber('0');
+      setFirstNumber(currentNumber);
+
+      setOperation(op);
+    }
+
+    if(op === '%'){
+      setCurrentnumber(String((Number(currentNumber) / 100)));
+    }
+  }
+
+  const handlerAddNumber = (num) => {
+    setCurrentnumber(prev => `${prev === '0' ?  '' : prev }${num}`);
+  }
+
+  const clearNumber = () => {
+    setCurrentnumber('0');
+    setFirstNumber('0')
+    setOperation('');
+  }
+
+  const Calcular = () => {
+    console.log('currentNumber');
+    if(!(firstNumber === '0' && currentNumber === '0'))
+    {
+      switch(operation){
+        case '-': 
+          setCurrentnumber(String(Number(firstNumber.replace(",", ".")) - Number(currentNumber.replace(",","."))));
+          break;
+        case '+': 
+          setCurrentnumber(String(Number(firstNumber.replace(",",".")) + Number(currentNumber.replace(",","."))));
+          break;
+        case '/': 
+          setCurrentnumber(String(Number(firstNumber.replace(",",".")) / Number(currentNumber.replace(",","."))));
+        break;
+        case '*': 
+          setCurrentnumber(String(Number(firstNumber.replace(",",".")) * Number(currentNumber.replace(",","."))));
+        break;  
+        case '%': 
+          setCurrentnumber(String(Number(firstNumber.replace(",",".")) * Number(currentNumber.replace(",","."))));
+        break;  
+        case '√': 
+          setCurrentnumber(String( Math.pow(Number(firstNumber.replace(",",".")),  Number(currentNumber.replace(",",".")))));
+        break;  
+        default:
+          setCurrentnumber('0');
+          setFirstNumber('0')
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Content>
+        <Input value={currentNumber}></Input>  
+        <Row>
+          <Button label="÷" onClick={() => {addOperation('/')}}></Button>
+          <Button label="x" onClick={() => {addOperation('*')}}></Button>
+          <Button label="%" onClick={() => {addOperation('%')}}></Button>
+          <Button label="√" onClick={() => {addOperation('√')}}></Button>
+        </Row>
+        <Row></Row>
+        <Row>
+          <Button label="7" onClick={() => {handlerAddNumber(7)}}></Button>
+          <Button label="8" onClick={() => {handlerAddNumber(8)}}></Button>
+          <Button label="9" onClick={() => {handlerAddNumber(9)}}></Button>
+          <Button label="-" onClick={() => {addOperation('-')}}></Button>
+        </Row>
+        <Row>
+          <Button label="4"  onClick={() => {handlerAddNumber(4)}}></Button>
+          <Button label="5"  onClick={() => {handlerAddNumber(5)}}></Button>
+          <Button label="6"  onClick={() => {handlerAddNumber(6)}}></Button>
+          <Button label="+" onClick={() => {addOperation('+')}}></Button>
+        </Row>
+        <Row>
+          <Button label="1"  onClick={() => {handlerAddNumber(1)}}></Button>
+          <Button label="2"  onClick={() => {handlerAddNumber(2)}}></Button>
+          <Button label="3"  onClick={() => {handlerAddNumber(3)}}></Button>
+          <Button label="C" onClick={clearNumber}></Button>
+        </Row>
+        <Row>
+          <Button label="0"  onClick={() => {handlerAddNumber(0)}}></Button>
+          <Button label="00"  onClick={() => {handlerAddNumber('00')}}></Button>
+          <Button label="," onClick={() => {handlerAddNumber(',')}}></Button>
+          <Button label="=" onClick={Calcular}></Button>
+        </Row>
+        
+        
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+
+    display: flex; 
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        padding: 0 10px;
+        font-size: 24px;
+        text-align: right;
+        color: #FFFFFFFF
+    }
+`
